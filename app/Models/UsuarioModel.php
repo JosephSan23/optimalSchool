9933ce8cfa66fad1e6b319107adc6400d681b2e5
Joseph Olarte <josephsantiago2345@gmail.com>
2025-11-11 11:02:53 -0500

    Agregando el modelo de usuarios

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -1,20 +1,38 @@
-<?php
+<?php 
 namespace App\Models;
 use CodeIgniter\Model;
 
-class UsuarioModel extends Model
-{
+class UsuarioModel extends Model {
     protected $table = 'usuario';
     protected $primaryKey = 'id_usuario';
-    protected $allowFields = ['id_usuario', 'nombre_usu',
-     'pass_usuario','id_perfil','ced_empleado'];
-    public $timestamps = false;
+    protected $allowedFields = ['id_usuario', 'colegio_id','rol', 'primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'tipo_documento', 'documento', 'correo', 'telefono', 'direccion', 'fecha_nacimiento', 'contrasena'];
 
-    public function VerificarUsuario($usuario, $password)
-    { 
-        return $this->where('nombre_usu', $usuario)
-                    ->where('pass_usuario',$password)
-                    ->first();
+    // obtener usuario por ID
+    public function obtenerPorId($id) {
+        return $this->asArray()->find($id);
+    }
+
+    // Buscar por documento
+    public function buscarPorDocumento($documento) {
+        return $this->where('documento', $documento)->first();
+    }
+
+    // Crear Usuario (retorna id insertado o false)
+    public function crearUsuario($data) {
+        $this->insert($data);
+        return $this->getInsertID();
+    }
+
+    // Actualizar usuario
+    public function actualizarUsuario(int $id, array $data)
+    {
+        return $this->update($id, $data);
+    }
+
+    // Eliminar usuario
+    public function eliminarUsuario(int $id)
+    {
+        return $this->delete($id);
     }
 }
 ?>
